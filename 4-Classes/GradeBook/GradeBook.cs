cc2dc0b9b6972a7110790ca6457bbfcdb3a8557e
Roberto Gomez <rob.77@live.com>
2020-10-30 09:28:21 -0500

    GradeBook

---
+++ 4-Classes/GradeBook/GradeBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+   public class GradeBook
+   {
+      // auto-implemented property CourseName implicitly creates an 
+      // instance variable for this GradeBook's course name
+      public string CourseName { get; set; }
+
+      // auto-implemented property InstructorName implicitly creates an 
+      // instance variable for this course's instructor
+      public string InstructorName { get; set; }
+
+      // constructor initializes automatic properties 
+      // with two strings supplied as arguments
+      public GradeBook(string course, string instructor)
+      {
+         CourseName = course; // set CourseName to course
+         InstructorName = instructor; // set InstructorName to instructor
+      } // end constructor
+
+      // display a welcome message to the GradeBook user
+      public void DisplayMessage()
+      {
+         // this statement uses automatic properties CourseName and 
+         // InstructorName to get the name of the course and instructor
+         Console.WriteLine("Welcome to the grade book for\n{0}!",
+            CourseName);
+         Console.WriteLine("This course is presented by: {0}",
+            InstructorName);
+      } // end method DisplayMessage
+   } // end class GradeBook
+}
---
+++ 4-Classes/GradeBook/GradeBookTest.cs
@@ -0,0 +1,28 @@
+﻿// Exercise 4.10 Solution: GradeBookTest.cs
+// GradeBook constructor used to specify the course name  
+// and instructor name at the time each GradeBook object is created.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+   public class GradeBookTest
+   {
+      // Main method begins program execution
+      public static void Main(string[] args)
+      {
+         // create GradeBook object
+         GradeBook gradeBook1 = new GradeBook(
+            "CS101 Introduction to C# Programming", "Sam Smith");
+
+         gradeBook1.DisplayMessage(); // display welcome message
+
+         Console.WriteLine("\nChanging instructor name to Judy Jones\n");
+         gradeBook1.InstructorName = "Judy Jones";
+
+         gradeBook1.DisplayMessage(); // display welcome message
+      } // end Main
+   } // end class GradeBookTest
+}
