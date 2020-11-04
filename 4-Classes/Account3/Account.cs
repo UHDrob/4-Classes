d6dcf120b8c488c15addce715c9fa2bcaff3980b
Roberto Gomez <rob.77@live.com>
2020-11-04 12:59:34 -0600

    Account 3

---
+++ 4-Classes/Account3/Account.cs
@@ -0,0 +1,20 @@
+﻿// Fig. 4.8: Account.cs
+// Account class with a constructor that initializes an Account's name.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account3
+{
+   class Account
+   {
+      public string Name { get; set; } // auto-implemented property
+
+      // constructor sets the Name property to parameter accountName's value
+      public Account(string accountName) // constructor name is class name 
+      {
+         Name = accountName;
+      }
+   }
+}
---
+++ 4-Classes/Account3/AccountTest.cs
@@ -0,0 +1,24 @@
+﻿// Fig. 4.9: AccountTest.cs
+// Using the Account constructor to set an Account's name
+// when an Account object is created.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Account3
+{
+   class AccountTest
+   {
+      static void Main()
+      {
+         // create two Account objects
+         Account account1 = new Account("Jane Green");
+         Account account2 = new Account("John Blue");
+
+         // display initial value of name for each Account
+         Console.WriteLine($"account1 name is: {account1.Name}");
+         Console.WriteLine($"account2 name is: {account2.Name}");
+      }
+   }
+}
