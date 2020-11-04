6e98657fda6f82f3cd5837d268de7824ed9908cf
Roberto Gomez <rob.77@live.com>
2020-11-04 11:14:22 -0600

    Account 1

---
+++ 4-Classes/4-2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Fig 4.1 AccountTest.cs
+// Creating and manipulating an Account object
+
+using System;
+
+namespace _4_2
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello World!");
+      }
+   }
+}
