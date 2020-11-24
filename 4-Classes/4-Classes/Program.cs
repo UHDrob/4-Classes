25d372c5edac253ae37e69367f5f8741277e31cc
Roberto Gomez <rob.77@live.com>
2020-11-24 13:33:15 -0600

    Chapter 4 Menu

--- 4-Classes/4-Classes/Program.cs
+++ 4-Classes/4-Classes/Program.cs
@@ -7,6 +7,21 @@ namespace _4_Classes
       static void Main(string[] args)
       {
          Console.WriteLine("Chapter 4 World!");
+         Console.WriteLine("\nEXAMPLES");
+         Console.WriteLine("Account 1: 4.1   4.2");
+         Console.WriteLine("Account 2: 4.5   4.6");
+         Console.WriteLine("Account 3: 4.8   4.9");
+         Console.WriteLine("Account 4: 4.11  4.12");
+
+            Console.WriteLine("\nEXERCISES");
+
+            Console.WriteLine("Exercise 4 10: GradeBook");
+            Console.WriteLine("Exercise 4 11: Account");
+            Console.WriteLine("Exercise 4 12: Invoice");
+            Console.WriteLine("Exercise 4 13: Employee");
+            Console.WriteLine("Exercise 4 14: Date");
+            Console.WriteLine("Exercise 4 15: HeartRates");
+            Console.WriteLine("Exercise 4 16: HealthProfile");
       }
    }
 }
--- 4-Classes/GradeBook/GradeBook.cs
+++ 4-Classes/GradeBook/GradeBook.cs
@@ -6,31 +6,31 @@ namespace GradeBook
 {
    public class GradeBook
    {
-      // auto-implemented property CourseName implicitly creates an 
-      // instance variable for this GradeBook's course name
-      public string CourseName { get; set; }
+        // auto-implemented property CourseName implicitly creates an 
+        // instance variable for this GradeBook's course name
+        public string CourseName { get; set; }
 
-      // auto-implemented property InstructorName implicitly creates an 
-      // instance variable for this course's instructor
-      public string InstructorName { get; set; }
+        // auto-implemented property InstructorName implicitly creates an 
+        // instance variable for this course's instructor
+        public string InstructorName { get; set; }
 
-      // constructor initializes automatic properties 
-      // with two strings supplied as arguments
-      public GradeBook(string course, string instructor)
-      {
-         CourseName = course; // set CourseName to course
-         InstructorName = instructor; // set InstructorName to instructor
-      } // end constructor
+        // constructor initializes automatic properties 
+        // with two strings supplied as arguments
+        public GradeBook(string course, string instructor)
+        {
+            CourseName = course; // set CourseName to course
+            InstructorName = instructor; // set InstructorName to instructor
+        } // end constructor
 
-      // display a welcome message to the GradeBook user
-      public void DisplayMessage()
-      {
+        // display a welcome message to the GradeBook user
+        public void DisplayMessage()
+        {
          // this statement uses automatic properties CourseName and 
          // InstructorName to get the name of the course and instructor
          Console.WriteLine("Welcome to the grade book for\n{0}!",
             CourseName);
          Console.WriteLine("This course is presented by: {0}",
             InstructorName);
-      } // end method DisplayMessage
-   } // end class GradeBook
+        } // end method DisplayMessage
+    } // end class GradeBook
 }
